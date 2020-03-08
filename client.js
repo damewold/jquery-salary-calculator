1c919b51ed1dec47d08831680449bb73322c12b0
Denis Woldemariam <damewold@gmail.com>
2020-03-07 18:29:28 -0600

    inserted jquery.js,bootstrap and style.css

---
+++ client.js
@@ -0,0 +1,26 @@
+console.log('js');//check client.js is sourced correctly
+
+$(document).ready(onReady);
+
+function onReady(){
+    //check jquery.js sourced correctly
+    console.log('Hello!');
+    collectFormInfo();
+};
+
+//need input form that collects _employee first name, last name, ID number, job title, annual salary_.
+
+let formInfoArray =[];
+let formInfoObject={
+       firstName:'First Name',
+        lastName:'Last Name',
+        idNumber:'ID Number',
+        jobTitle:'Job Title',
+    annualSalary:'Annual Salary'
+};
+
+function collectFormInfo (){
+ console.log('in collectFormInfo')
+
+
+};
