db52034197a9c286ac3844cbe87368c757ab2cd8
Denis Woldemariam <damewold@gmail.com>
2020-03-07 20:34:49 -0600

    wrote displayFormInfo function

--- client.js
+++ client.js
@@ -1,30 +1,49 @@
 console.log('js');//check client.js is sourced correctly
-
 $(document).ready(onReady);
+//write down element selectors to be used
+let firstName=$('#firstName');
+let lastName=$('#lastName');
+let idNumber=$('#idNumber');
+let jobTitle=$('#jobTitle');
+let annualSalary=$('#annualSalary');
 
 function onReady(){
     //check jquery.js sourced correctly
     console.log('Hello!');
     collectFormInfo();
+    $('#addButton').on('click', collectFormInfo());
+    $('#addButton').on('click', displayFormInfo());
+
 };
 
 //need input form that collects _employee first name, last name, ID number, job title, annual salary_.
 
 let formInfoArray =[];
-let formInfoObject={
-       firstName:'First Name',
-        lastName:'Last Name',
-        idNumber:'ID Number',
-        jobTitle:'Job Title',
-    annualSalary:'Annual Salary'
+
+function collectFormInfo (firstName,lastName, idNumber, jobTitle, annualSalary){
+ console.log('in collectFormInfo: ',firstName,lastName, idNumber, jobTitle, annualSalary );
+ let formInfoObject={
+    firstName:$('#firstName').val(),
+     lastName:$('#lastName').val(),
+     idNumber:$('#idNumber').val(),
+     jobTitle:$('#jobTitle').val(),
+ annualSalary:$('#annualSalary').val(),
 };
 
-function collectFormInfo (formInfoObject){
- console.log('in collectFormInfo')
  //collect form info 
 for (i=0;i<formInfoArray.length;i++){
    formInfoArray.push(formInfoObject);
 };
+//empty out the values 
+ $('#firstName').val(' ');
+ $('#lastName').val(' ');
+ $('#idNumber').val(' ');
+ $('#jobTitle').val(' ');
+ $('#annualSalary').val(' ');
 
+};
+
+function displayFormInfo(){
+    console.log('in displayFormInfo');
 
 };
