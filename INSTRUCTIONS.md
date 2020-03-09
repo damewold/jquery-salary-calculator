486997739c0dd65a066a2d396d8272a7dce437ee
Denis Woldemariam <damewold@gmail.com>
2020-03-09 11:52:39 -0500

    made some adjustment to the readme file

--- INSTRUCTIONS.md
+++ INSTRUCTIONS.md
@@ -1,3 +1,19 @@
+![MIT LICENSE](https://img.shields.io/github/license/scottbromander/the_marketplace.svg?style=flat-square)
+![REPO SIZE](https://img.shields.io/github/repo-size/scottbromander/the_marketplace.svg?style=flat-square)
+![TOP_LANGUAGE](https://img.shields.io/github/languages/top/scottbromander/the_marketplace.svg?style=flat-square)
+![FORKS](https://img.shields.io/github/forks/scottbromander/the_marketplace.svg?style=social)
+
+# Employee Salary Calculator
+
+## Description
+_Duration: 12 hours_
+
+This project aims at developing an application that records employee salaries up to report monthly costs. Using the information provided through the inputs, it will calculate and report the total monthly costs. 
+
+The application  takes in five inputs. These are _employee first name, last name, ID number, job title, annual salary_.
+
+It will also signal when the total monthly cost exceeds $20,000.
+
 # Weekend Challenge: jQuery Salary Calculator
 Create an application that records employee salaries and adds salaries up to report monthly costs. 
 
--- client.js
+++ client.js
@@ -11,36 +11,36 @@ let annualSalary=$('#annualSalary');
 function onReady(){
     //check jquery.js sourced correctly
     console.log('Hello!');
-        
+    onlyDisplay();
     $('#button').on('click', collectFormInfo);
     $('#button').on('click', displayFormInfo);
-  
+    totalMonthly();
     
-
 };
 
 //need input form that collects _employee first name, last name, ID number, job title, annual salary_.
 
 let formInfoArray =[];
 
-function collectFormInfo ({firstName,lastName,idNumber,jobTitle,annualSalary}){
- console.log('in collectFormInfo: ',{firstName,lastName,idNumber,jobTitle,annualSalary});
- const formInfoObject={
-    firstName:$('#firstName').val(),
-     lastName:$('#lastName').val(),
-     idNumber:$('#idNumber').val(),
-     jobTitle:$('#jobTitle').val(),
- annualSalary:$('#annualSalary').val()
-};
+function collectFormInfo (){
+//prevent page from refreshing due to form tag
+    event.preventDefault();  
+ console.log('in collectFormInfo');
+ let firstName=$('#firstName').val();
+ let lastName=$('#lastName').val();
+  let idNumber=$('#idNumber').val();
+  let jobTitle=$('#jobTitle').val();
+ let annualSalary=$('#annualSalary').val()
+
  //collect form info 
- formInfoArray.push(formInfoObject);
+ formInfoArray.push(({firstName,lastName,idNumber,jobTitle,annualSalary}));
 
 //empty out the values
-$('#firstName').val(' ')
-$('#lastName').val(' ')
-$('#idNumber').val(' ')
-$('#jobTitle').val(' ')
-$('#annualSalary').val(' ')
+$('#firstName').val(' ');
+$('#lastName').val(' ');
+$('#idNumber').val(' ');
+$('#jobTitle').val(' ');
+$('#annualSalary').val(' ');
 console.log('Objects in formInfoArray', formInfoArray)
 
 };
@@ -48,9 +48,7 @@ console.log('Objects in formInfoArray', formInfoArray)
 
 function displayFormInfo(){
     console.log('in displayFormInfo');
-//prevent page from refreshing due to form tag
-    event.preventDefault();  
-//set 
+//define the table
     let table=$('#containerTable');
     let tbody = $('<tbody id="containerTableBody"></tbody>');
     table.append(tbody);
@@ -59,8 +57,7 @@ function displayFormInfo(){
 $(tbody).empty();
   let row = $('<tr><td></td></tr>')
   row.empty();
-  for (i=0;i<formInfoArray.length;i++){
-      if(formInfoArray.length>0){    
+  for (i=0;i<formInfoArray.length;i++){    
   tbody.append(`<tr>
                 <td>${formInfoArray[i].firstName}</td>,
                 <td>${formInfoArray[i].lastName}</td>,
@@ -69,28 +66,27 @@ $(tbody).empty();
                 <td>${formInfoArray[i].annualSalary}</td>
                 <td><button id="delButton">Delete</button></td>
                 </tr>`);
-      };
 
 };
 }
 //create a function that will prevent addition of emppty objects
-// function onlyDisplay(){
-//       if(formInfoArray.length>0){
-//         displayFormInfo();
-//       };
+function onlyDisplay(){
+      if(formInfoArray.length>0){
+        displayFormInfo();
+      };
 
-//     };
+    };
 //create a function for total monthly cost
 
 function totalMonthly(){
- ('in totalMonthly');
+ console.log('in totalMonthly');
  //initialize total Monthly salary
  let totalMonthlySum=$('#totalMonthly');
  //set total Monthly cost equal to zero
  let totalMonthlyCost=0
  //write for loop with a conditional
  for(i=0;i<formInfoArray.length;i++){
-     totalMonthlyCost+=Number(formInfoArray[i].annualSalary/12);
+    Number(totalMonthlyCost)+=Number(formInfoArray[i].annualSalary/12);
      if(totalMonthlyCost>20000){
          $('#totalMonthly').css("background-color","red");
      };
@@ -100,6 +96,7 @@ function totalMonthly(){
  //append totalMonthlyCost to totalMonthlySum
  totalMonthlySum.append(totalMonthlyCost);
 };
+
 
 //delete a whole row function
  $(document).on('click', '#delButton', function(e){
